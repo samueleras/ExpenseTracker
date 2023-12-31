a6fb98c094337bf363999191f480419e193c46cb
Samuel Eras <sa.eras@gmx.de>
2023-12-31 14:46:59 +0100

    implement components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import "./App.css";
 import TrackingForm from "./components/TrackingForm";
 import ExpensesDisplayer from "./components/ExpensesDisplayer";
+import { useState } from "react";
 
 function App() {
+  interface ExpenseItem {
+    description: string;
+    amount: number;
+    category: number;
+  }
+
+  const [expenseList, setExpenseList] = useState<ExpenseItem[]>([]);
+
+  const onSubmit = (item: ExpenseItem) => {
+    setExpenseList([...expenseList, item]);
+    console.log(expenseList);
+  };
+
+  const onDelete = () => {};
+
   return (
     <>
       <h1>ExpenseTracker</h1>
-      <TrackingForm />
-      <ExpensesDisplayer />
+      <TrackingForm onSubmit={onSubmit} />
+      <ExpensesDisplayer onDelete={onDelete} expenseList={expenseList} />
     </>
   );
 }
