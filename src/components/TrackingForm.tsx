604aded0fd553f368e7f150417ea3e33dffb99f6
Samuel Eras <sa.eras@gmx.de>
2023-12-31 13:56:11 +0100

    create component TrackingForm

---
+++ src/components/TrackingForm.tsx
@@ -0,0 +1,81 @@
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  description: z
+    .string()
+    .min(3, { message: "Description should be at least 3 characters" }),
+  amount: z
+    .number({ invalid_type_error: "Amount is required." })
+    .min(1, { message: "Amount must be at least 1." }),
+  category: z.number({ invalid_type_error: "Category is required." }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+const TrackingForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">
+          Description
+        </label>
+        <input
+          {...register("description")}
+          id="description"
+          type="text"
+          className="form-control"
+        />
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
+          type="number"
+          className="form-control"
+        />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <select
+          {...register("category", { valueAsNumber: true })}
+          id="category"
+          className="form-select"
+        >
+          <option selected>Select</option>
+          <option value="1">Groceries</option>
+          <option value="2">Utilities</option>
+          <option value="3">Entertainment</option>
+        </select>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default TrackingForm;
